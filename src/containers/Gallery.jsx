58475ba2a30a0f99471d8807c819ca5d994ca0f0
Lisbaldy <lisba7474@gmail.com>
2020-10-24 13:01:41 -0300

    Gallery improved

--- src/containers/Gallery.jsx
+++ src/containers/Gallery.jsx
@@ -4,20 +4,11 @@ import { Carousel } from 'react-responsive-carousel';
 
 const Gallery = () => {
 
-    const images = [
-        'assets/static/2-people-sitting-during-daytime.jpg',
-        'assets/static/action-adult-adventure-beautiful.jpg',
-        'assets/static/amsterdam-arch-bridge-architecture.jpg',
-        'assets/static/art-beach-beautiful-clouds.jpg',
-        'assets/static/backlit-beach-dawn-dusk.jpg',
-        'assets/static/brown-hut-island-gazebo.jpg',
-        'assets/static/reconectando con la naturaleza.jpg',
-        'assets/static/cafe-camera.jpg',
-        'assets/static/close-up-of-pictures.jpg',
-        'assets/static/green-car-near-blue-ocean.jpg',
-        'assets/static/paris-eiffel-tower.jpg',
-        'assets/static/photography-of-airplane-during-sunrise.jpg'
-    ];
+    let images = [];
+
+    for(let i=1; i<19; i++){
+        images.push(`assets/static/galeria${i}.jpg`);
+    }
 
     return(
         <div className="gallery">
